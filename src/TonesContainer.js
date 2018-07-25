1cbdf48f6e4b006debf0a73063923fb86a65ad95
James Reichard <jpqreichard@gmail.com>
2018-07-24 21:32:20 -0400

    Maintain playback during changes in pitch set

--- src/TonesContainer.js
+++ src/TonesContainer.js
@@ -15,29 +15,32 @@ export default class TonesContainer extends Component {
       activeTones: TONES,
       activeCount: TONES.length,
       offset: 2,
-      playing: false
+      playing: false,
+      pattern: null
     }
     this.transport = Tone.Transport
-    this.pattern = null
   }
 
   play = () => {
-    this.setState(prevState => {
-      if (this.pattern) this.pattern.dispose()
-      const tones = prevState.activeTones.map(tone => `${tone}4`)
-      this.pattern = new Tone.Pattern((time, note) => {
-        this.props.polySynth.triggerAttackRelease(note, '2n')
-      }, tones)
-      this.pattern.start(0)
-      this.transport.start()
-      return {
-        playing: true
-      }
-    })
+    this.setState(
+      prevState => {
+        if (prevState.pattern) prevState.pattern.dispose()
+        const tones = prevState.activeTones.map(tone => `${tone}4`)
+        const pattern = new Tone.Pattern((time, note) => {
+          this.props.polySynth.triggerAttackRelease(note, '2n')
+        }, tones)
+        pattern.start(0)
+        return {
+          pattern,
+          playing: true
+        }
+      },
+      _ => this.transport.start()
+    )
   }
 
   stop = () => {
-    this.setState(prevState=>{
+    this.setState(prevState => {
       this.transport.stop()
       return {
         playing: false
@@ -46,12 +49,15 @@ export default class TonesContainer extends Component {
   }
   
   filterTones = () => {
-    this.setState(({tones, offset, activeCount}) => {
-      const pattern = rotate(generatePattern(activeCount, tones.length), offset)
-      return {
-        activeTones: tones.filter((_, index) => pattern[index])
-      }
-    })
+    this.setState(
+      ({tones, offset, activeCount}) => {
+        const pattern = rotate(generatePattern(activeCount, tones.length), offset)
+        return {
+          activeTones: tones.filter((_, index) => pattern[index])
+        }
+      }, 
+      this.state.playing ? this.play : this.stop
+    )
   }
 
   handleChange = event => {
